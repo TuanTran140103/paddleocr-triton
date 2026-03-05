ef56dce606a7870e4666c504ea49c690a7db9ff0
tuantranquoc140103-vn <tuantranquoc140103@gmail.com>
2026-03-05 15:26:18 +0700

    first init

---
+++ prepare.sh
@@ -0,0 +1,12 @@
+# #!/usr/bin/env bash
+
+# wget https://paddle-model-ecology.bj.bcebos.com/paddlex/PaddleX3.0/deploy/paddlex_hps/public/sdks/v3.4/paddlex_hps_PaddleOCR-VL_sdk.tar.gz
+# tar -xf paddlex_hps_PaddleOCR-VL_sdk.tar.gz
+# cp ../pipeline_config_vllm.yaml paddlex_hps_PaddleOCR-VL_sdk/server/pipeline_config.yaml
+#!/usr/bin/env bash
+
+# Thay wget bằng curl
+curl -O https://paddle-model-ecology.bj.bcebos.com/paddlex/PaddleX3.0/deploy/paddlex_hps/public/sdks/v3.4/paddlex_hps_PaddleOCR-VL_sdk.tar.gz
+
+tar -xf paddlex_hps_PaddleOCR-VL_sdk.tar.gz
+cp ../pipeline_config_vllm.yaml paddlex_hps_PaddleOCR-VL_sdk/server/pipeline_config.yaml
